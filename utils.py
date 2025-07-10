ba86ceb8cc3952f8bdf524e4701704b0c7a1438f
Yunhao Luo <86722488+Yunhao-Luo@users.noreply.github.com>
2025-07-10 11:07:41 -0700

    Revert "Adding images and order"

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import json
-import requests
 
 def read_data():
     file_path = "AI_output" + "/dummy.json"
@@ -14,8 +13,3 @@ def load_explanation(scenario_num, framework):
         data = json.load(file)
 
     return data[scenario_num][framework]
-
-def get_participant_num():
-    response = requests.get("http://128.111.28.83:8000/next_participant")
-    participant_id = response.json()["participant_number"]
-    return participant_id
